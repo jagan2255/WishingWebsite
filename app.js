a11ed49cd0879ed578794018354df3baf14e2161
J V <jaganvarah22@gmail.com>
2022-07-05 18:05:53 +0530

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const  nodeMailer = require('nodemailer');
 
 
-const PORT = process.env.process || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
