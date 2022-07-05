968a8c84e99ac9f7ae2a088766cf90e4c14976f2
J V <jaganvarah22@gmail.com>
2022-07-05 16:19:31 +0530

    make it better

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine','ejs');
 app.set('views','./src/views');
 
 app.get("/" , (req,res)=>{
-    res.render("index");
+    res.render('index');
 })
 
 app.post("/post" , (req,res)=>{
